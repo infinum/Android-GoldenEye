c55a7ebcc7630d0f6560673bf78cfed075570ef3
Tehila <tehilaschechter@gmail.com>
2020-01-22 15:31:24 +0200

    this version works

--- example/src/main/java/co/infinum/example/MainActivity.kt
+++ example/src/main/java/co/infinum/example/MainActivity.kt
@@ -91,7 +91,7 @@ class MainActivity : AppCompatActivity() {
             goldenEye.takePicture(
                 onPictureTaken = { bitmap ->
                     if (bitmap.width <= 4096 && bitmap.height <= 4096) {
-                        saveImage(bitmap, "MyExperience", this)//todo need unique title
+                        saveImage(bitmap, "MyExperience" + java.util.Calendar.getInstance(), this)
                         displayPicture(bitmap)
                     } else {
                         reducePictureSize(bitmap)
@@ -271,57 +271,6 @@ class MainActivity : AppCompatActivity() {
         }
     }
 }
-
-/*private fun saveImage(bitmap: Bitmap, context: Context, folderName: String) {
-    if (android.os.Build.VERSION.SDK_INT >= 29) {
-        val values = contentValues()
-        values.put(MediaStore.Images.Media.RELATIVE_PATH, "Pictures/" + folderName)
-        values.put(MediaStore.Images.Media.IS_PENDING, true)
-        // RELATIVE_PATH and IS_PENDING are introduced in API 29.
-
-        val uri: Uri? = context.contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values)
-        if (uri != null) {
-            saveImageToStream(bitmap, context.contentResolver.openOutputStream(uri))
-            values.put(MediaStore.Images.Media.IS_PENDING, false)
-            context.contentResolver.update(uri, values, null, null)
-        }
-    } else {
-        val directory = File(Environment.getExternalStorageDirectory().toString() + separator + folderName)
-        // getExternalStorageDirectory is deprecated in API 29
-
-        if (!directory.exists()) {
-            directory.mkdirs()
-        }
-        val fileName = System.currentTimeMillis().toString() + ".png"
-        val file = File(directory, fileName)
-        saveImageToStream(bitmap, FileOutputStream(file))
-        if (file.absolutePath != null) {
-            val values = contentValues()
-            values.put(MediaStore.Images.Media.DATA, file.absolutePath)
-            // .DATA is deprecated in API 29
-            context.contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values)
-        }
-    }
-}
-
-private fun contentValues() : ContentValues {
-    val values = ContentValues()
-    values.put(MediaStore.Images.Media.MIME_TYPE, "image/png")
-    values.put(MediaStore.Images.Media.DATE_ADDED, System.currentTimeMillis() / 1000);
-    values.put(MediaStore.Images.Media.DATE_TAKEN, System.currentTimeMillis());
-    return values
-}
-
-private fun saveImageToStream(bitmap: Bitmap, outputStream: OutputStream?) {
-    if (outputStream != null) {
-        try {
-            bitmap.compress(Bitmap.CompressFormat.PNG, 100, outputStream)
-            outputStream.close()
-        } catch (e: Exception) {
-            e.printStackTrace()
-        }
-    }
-}*/
 
 // Method to save an image to gallery and return uri
 private fun saveImage(bitmap:Bitmap, title:String, context:Context):Uri{
