3419095e529a8c828edffee07c37cc73cc5a8b9b
pavecic <mario.pavecic@infinum.hr>
2020-06-03 12:41:07 +0200

    Add camerax additional settings

--- example/src/main/java/co/infinum/example/CameraXConfigExtensions.kt
+++ example/src/main/java/co/infinum/example/CameraXConfigExtensions.kt
@@ -1,11 +1,61 @@
 package co.infinum.example
 
+import android.content.Context
+import android.util.Size
+import android.view.View
+import android.view.ViewTreeObserver
+import androidx.appcompat.app.AlertDialog
 import androidx.camera.core.AspectRatio
 import androidx.camera.core.ImageCapture
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+val FOCUS_MODE_AUTO = 0
+val FOCUS_MODE_TAP = 1
+val FOCUS_MODE_OFF = 2
+
+fun suportedPictureSizes() = listOf<Size?>(
+    null,
+    Size(8000, 6000),
+    Size(4000, 3000),
+    Size(4000, 2250),
+    Size(3840, 2160),
+    Size(1440, 1440)
+)
+
+fun supportedFlashModes() = listOf<Int>(ImageCapture.FLASH_MODE_AUTO, ImageCapture.FLASH_MODE_ON, ImageCapture.FLASH_MODE_OFF)
+
+fun supportedFocusModes() = listOf<Int>(FOCUS_MODE_AUTO, FOCUS_MODE_TAP, FOCUS_MODE_OFF)
+
+fun Size.convertToString() = "$width x $height"
+fun flashModeToString(flashMode: Int) = when (flashMode) {
+    ImageCapture.FLASH_MODE_AUTO -> "auto"
+    ImageCapture.FLASH_MODE_ON -> "on"
+    else -> "off"
+}
+
+fun Int.aspectRatioToString() = if (this == 1) "16x9" else "4x3"
+
+fun View.afterMeasured(block: () -> Unit) {
+    viewTreeObserver.addOnGlobalLayoutListener(object : ViewTreeObserver.OnGlobalLayoutListener {
+        override fun onGlobalLayout() {
+            if (measuredWidth > 0 && measuredHeight > 0) {
+                viewTreeObserver.removeOnGlobalLayoutListener(this)
+                block()
+            }
+        }
+    })
+}
 
 data class CameraXConfig(
-    @ImageCapture.FlashMode val flashMode: Int? = null,
-    @AspectRatio.Ratio val aspectRatio: Int? = null
+    val previewSize: Size = Size(1440, 1080),
+    @AspectRatio.Ratio var aspectRatio: Int = AspectRatio.RATIO_16_9,
+    var pictureSize: Size? = null,
+//    var videoFrameRate: Int? = null,
+//    var videoStabilicationEnabled: Boolean = false,
+    var pinchToZoomEnabled: Boolean = true,
+    @ImageCapture.FlashMode var flashMode: Int = ImageCapture.FLASH_MODE_AUTO,
+    var focusMode: Int = FOCUS_MODE_AUTO
 )
 
 fun ImageCapture.Builder.applyConfigs(
@@ -15,5 +65,173 @@ fun ImageCapture.Builder.applyConfigs(
     apply {
         flashMode?.let { setFlashMode(it) }
         aspectRatio?.let { setTargetAspectRatio(it) }
+    }
+}
+
+fun CameraXConfig.prepareItems(context: Context, adapter: SettingsAdapter) {
+    val settingsItems = listOf(
+        SettingsItem(name = "Basic features", type = 1),
+        // TODO preview size is screen size
+        SettingsItem("Preview size:", "YYYY x YYYY") {
+            context.toast("Preview scale is automatic so it picks preview size on its own.")
+        },
+        SettingsItem("Picture size:", pictureSize?.convertToString() ?: "null") {
+            displayDialog(
+                context = context,
+                config = this,
+                settingsAdapter = adapter,
+                title = "Picture size",
+                listItems = suportedPictureSizes().map { ListItem(it, it?.convertToString() ?: "null") },
+                onClick = {
+                    pictureSize = it
+                }
+            )
+        },
+        SettingsItem("Preview scale", aspectRatio.aspectRatioToString()) {
+            displayDialog(
+                context = context,
+                config = this,
+                settingsAdapter = adapter,
+                title = "Preview size",
+                listItems = listOf(
+                    ListItem(AspectRatio.RATIO_16_9, AspectRatio.RATIO_16_9.aspectRatioToString()),
+                    ListItem(AspectRatio.RATIO_4_3, AspectRatio.RATIO_4_3.aspectRatioToString())
+                ),
+                onClick = {
+                    aspectRatio = it
+                }
+            )
+        },
+//        SettingsItem("Video quality:", videoQuality.convertToString()) {
+//            displayDialog(
+//                context = context,
+//                config = this,
+//                settingsAdapter = adapter,
+//                title = "Video quality",
+//                listItems = supportedVideoQualities.map { ListItem(it, it.convertToString()) },
+//                onClick = { videoQuality = it }
+//            )
+//        },
+//        SettingsItem("Video stabilization:", videoStabilizationEnabled.convertToString()) {
+//            if (isVideoStabilizationSupported) {
+//                displayDialog(
+//                    context = context,
+//                    config = this,
+//                    settingsAdapter = adapter,
+//                    title = "Video stabilization",
+//                    listItems = boolList(),
+//                    onClick = { videoStabilizationEnabled = it }
+//                )
+//            } else {
+//                context.toast("Video stabilization not supported")
+//            }
+//        },
+        SettingsItem("Flash mode:", flashModeToString(flashMode)) {
+            displayDialog(
+                context = context,
+                config = this,
+                settingsAdapter = adapter,
+                title = "Flash mode",
+                listItems = supportedFlashModes().map { ListItem(it, flashModeToString(it)) },
+                onClick = {
+                    flashMode = it
+                }
+            )
+        },
+        SettingsItem("Focus mode:", focusMode.toString()) {
+            displayDialog(
+                context = context,
+                config = this,
+                settingsAdapter = adapter,
+                title = "Focus mode",
+                listItems = supportedFocusModes().map { ListItem(it, it.toString()) },
+                onClick = { focusMode = it }
+            )
+        },
+//        SettingsItem("Tap to focus - reset focus delay:", tapToFocusResetDelay.toString()) {
+//            if (isTapToFocusSupported) {
+//                displayDialog(
+//                    context = context,
+//                    config = this,
+//                    settingsAdapter = adapter,
+//                    title = "Reset delay",
+//                    listItems = listOf(
+//                        ListItem(2_500L, "2500"),
+//                        ListItem(5_000L, "5000"),
+//                        ListItem(7_500L, "7500")
+//                    ),
+//                    onClick = { tapToFocusResetDelay = it }
+//                )
+//            } else {
+//                context.toast("Tap to focus not supported.")
+//            }
+//        },
+        SettingsItem("Pinch to zoom:", pinchToZoomEnabled.toString()) {
+            displayDialog(
+                context = context,
+                config = this,
+                settingsAdapter = adapter,
+                title = "Pinch to zoom",
+                listItems = boolList(),
+                onClick = { pinchToZoomEnabled = it }
+            )
+        }
+//        SettingsItem(name = "Advanced features", type = 1),
+//        SettingsItem("White Balance:", whiteBalanceMode.convertToString()) {
+//            displayDialog(
+//                context = context,
+//                config = this,
+//                settingsAdapter = adapter,
+//                title = "White Balance",
+//                listItems = supportedWhiteBalanceModes.map { ListItem(it, it.convertToString()) },
+//                onClick = { whiteBalanceMode = it }
+//            )
+//        },
+//        SettingsItem("Color Effect:", colorEffectMode.convertToString()) {
+//            displayDialog(
+//                context = context,
+//                config = this,
+//                settingsAdapter = adapter,
+//                title = "Color Effect",
+//                listItems = supportedColorEffectModes.map { ListItem(it, it.convertToString()) },
+//                onClick = { colorEffectMode = it }
+//            )
+//        },
+//        SettingsItem("Antibanding:", antibandingMode.convertToString()) {
+//            displayDialog(
+//                context = context,
+//                config = this,
+//                settingsAdapter = adapter,
+//                title = "Antibanding",
+//                listItems = supportedAntibandingModes.map { ListItem(it, it.convertToString()) },
+//                onClick = { antibandingMode = it }
+//            )
+//        }
+    )
+    adapter.updateDataSet(settingsItems)
+}
+
+fun <T> displayDialog(
+    context: Context, config: CameraXConfig, settingsAdapter: SettingsAdapter,
+    title: String, listItems: List<ListItem<T>>, onClick: (T) -> Unit
+) {
+    if (listItems.isEmpty()) {
+        context.toast("$title not supported")
+        return
+    }
+
+    val dialog = AlertDialog.Builder(context)
+        .setView(R.layout.list)
+        .setCancelable(true)
+        .setTitle(title)
+        .show()
+
+    dialog.findViewById<RecyclerView>(R.id.recyclerView)?.apply {
+        layoutManager = LinearLayoutManager(context)
+        adapter = ListItemAdapter(listItems) {
+            onClick(it)
+            dialog.dismiss()
+            config.prepareItems(context, settingsAdapter)
+        }
     }
 }
--- example/src/main/java/co/infinum/example/MainActivity.kt
+++ example/src/main/java/co/infinum/example/MainActivity.kt
@@ -10,6 +10,7 @@ import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import android.util.Log
+import android.view.MotionEvent
 import android.view.ScaleGestureDetector
 import android.view.Surface
 import android.view.TextureView
@@ -17,20 +18,26 @@ import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.camera.core.Camera
+import androidx.camera.core.CameraInfoUnavailableException
 import androidx.camera.core.CameraSelector
 import androidx.camera.core.CameraX
+import androidx.camera.core.FocusMeteringAction
 import androidx.camera.core.ImageCapture
 import androidx.camera.core.ImageCaptureException
 import androidx.camera.core.ImageProxy
+import androidx.camera.core.MeteringPointFactory
 import androidx.camera.core.Preview
+import androidx.camera.core.SurfaceOrientedMeteringPointFactory
 import androidx.camera.core.VideoCapture
 import androidx.camera.core.impl.VideoCaptureConfig
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_main.*
 import java.io.File
 import java.util.concurrent.Executors
+import java.util.concurrent.TimeUnit
 import kotlin.math.min
 
 @SuppressLint("SetTextI18n", "RestrictedApi")
@@ -43,6 +50,7 @@ class MainActivity : AppCompatActivity() {
         private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
     }
 
+    private var configs: CameraXConfig = CameraXConfig()
     private var videoCapture: VideoCapture? = null
     private var imageCapture: ImageCapture? = null
     private var camera: Camera? = null
@@ -52,27 +60,27 @@ class MainActivity : AppCompatActivity() {
     private lateinit var videoFile: File
     private var isRecording = false
     private var cameraLensSelector = CameraSelector.DEFAULT_BACK_CAMERA
-//    private var settingsAdapter = SettingsAdapter(listOf())
+    private var settingsAdapter = SettingsAdapter(listOf())
+    private var needToRecreateCamera = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         videoFile = File.createTempFile("vid", "")
-
         initListeners()
-        setUpPinchToZoom()
+        setUpZoomAndFocus()
     }
 
     private fun initListeners() {
-//        settingsView.setOnClickListener {
-//            prepareItems()
-//            settingsRecyclerView.apply {
-//                visibility = View.VISIBLE
-//                layoutManager = LinearLayoutManager(this@MainActivity2)
-//                adapter = settingsAdapter
-//            }
-//        }
+        settingsView.setOnClickListener {
+            prepareItems()
+            settingsRecyclerView.apply {
+                visibility = View.VISIBLE
+                layoutManager = LinearLayoutManager(this@MainActivity)
+                adapter = settingsAdapter
+            }
+        }
 
         takePictureView.setOnClickListener { _ ->
             takePicture(
@@ -99,8 +107,6 @@ class MainActivity : AppCompatActivity() {
         switchCameraView.setOnClickListener { _ ->
             cameraLensSelector =
                 if (cameraLensSelector == CameraSelector.DEFAULT_BACK_CAMERA) CameraSelector.DEFAULT_FRONT_CAMERA else CameraSelector.DEFAULT_BACK_CAMERA
-            CameraX.getCameraWithCameraSelector(CameraSelector.DEFAULT_BACK_CAMERA)
-            CameraX.getCameraWithCameraSelector(cameraLensSelector)
             recreateCamera()
         }
     }
@@ -182,23 +188,37 @@ class MainActivity : AppCompatActivity() {
             val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
 
             // Preview
-            val preview = Preview.Builder()
-                .build()
-            imageCapture = ImageCapture.Builder()
-                .build()
-            videoCapture = VideoCaptureConfig.Builder().build()
+            val previewBuilder = Preview.Builder()
+            val imageCaptureBuilder = ImageCapture.Builder()
+                .setFlashMode(configs.flashMode)
+            val videoCaptureBuilder = VideoCaptureConfig.Builder()
+            if (configs.pictureSize != null) {
+                configs.pictureSize?.let {
+                    previewBuilder.setTargetResolution(it)
+                    imageCaptureBuilder.setTargetResolution(it)
+                    videoCaptureBuilder.setTargetResolution(it)
+                }
+            } else {
+                previewBuilder.setTargetAspectRatio(configs.aspectRatio)
+                imageCaptureBuilder.setTargetAspectRatio(configs.aspectRatio)
+                videoCaptureBuilder.setTargetAspectRatio(configs.aspectRatio)
+            }
+            val preview = previewBuilder.build()
+            imageCapture = imageCaptureBuilder.build()
+            videoCapture = videoCaptureBuilder.build()
 
             // Select camera lens
             val cameraSelector = cameraLensSelector
 
             try {
-                // Unbind use cases before rebinding
+                // Unbind use cases before rebindings
                 cameraProvider.unbindAll()
 
                 // Bind use cases to camera
                 camera = cameraProvider.bindToLifecycle(
                     this, cameraSelector, preview, imageCapture, videoCapture
                 )
+
                 preview.setSurfaceProvider(viewFinder.createSurfaceProvider(camera?.cameraInfo))
             } catch (exc: Exception) {
                 Log.e(TAG, "Use case binding failed", exc)
@@ -262,15 +282,19 @@ class MainActivity : AppCompatActivity() {
     override fun onBackPressed() {
         if (settingsRecyclerView.visibility == View.VISIBLE) {
             settingsRecyclerView.visibility = View.GONE
+            updateSettings()
         } else {
             super.onBackPressed()
         }
     }
 
+    private fun updateSettings() {
+        recreateCamera()
+        setUpZoomAndFocus()
+    }
+
     private fun prepareItems() {
-//        camera?.cameraControl.
-//            prepareItems(this@MainActivity, settingsAdapter)
-//        }
+        configs.prepareItems(this, settingsAdapter)
     }
 
     private fun startVideo() {
@@ -306,30 +330,96 @@ class MainActivity : AppCompatActivity() {
         ) == PackageManager.PERMISSION_GRANTED
     }
 
-    fun getOutputDirectory(): File {
-        val mediaDir = externalMediaDirs.firstOrNull()?.let {
-            File(it, resources.getString(R.string.app_name)).apply { mkdirs() }
-        }
-        return if (mediaDir != null && mediaDir.exists())
-            mediaDir else filesDir
+    private fun setUpPinchToZoom(event: MotionEvent, scaleGestureDetector: ScaleGestureDetector?): Boolean {
+        scaleGestureDetector?.onTouchEvent(event)
+        return true
     }
 
-    private fun setUpPinchToZoom() {
-        val listener = object : ScaleGestureDetector.SimpleOnScaleGestureListener() {
-            override fun onScale(detector: ScaleGestureDetector): Boolean {
-                val currentZoomRatio: Float = camera?.cameraInfo?.zoomState?.value?.zoomRatio ?: 0F
-                val delta = detector.scaleFactor
-                camera?.cameraControl?.setZoomRatio(currentZoomRatio * delta)
-                zoomView.text = "Zoom: %.02f".format(currentZoomRatio)
-                return true
+    private fun setAutomaticFocus() {
+        viewFinder.afterMeasured {
+            val factory: MeteringPointFactory = SurfaceOrientedMeteringPointFactory(
+                viewFinder.width.toFloat(), viewFinder.height.toFloat()
+            )
+            val centerWidth = viewFinder.width.toFloat() / 2
+            val centerHeight = viewFinder.height.toFloat() / 2
+            //create a point on the center of the view
+            val autoFocusPoint = factory.createPoint(centerWidth, centerHeight)
+            try {
+                camera?.cameraControl?.startFocusAndMetering(
+                    FocusMeteringAction.Builder(
+                        autoFocusPoint,
+                        FocusMeteringAction.FLAG_AF
+                    ).apply {
+                        //auto-focus every 1 seconds
+                        setAutoCancelDuration(1, TimeUnit.SECONDS)
+                    }.build()
+                )
+            } catch (e: CameraInfoUnavailableException) {
+                Log.d("ERROR", "cannot access camera", e)
             }
         }
+    }
 
-        val scaleGestureDetector = ScaleGestureDetector(this, listener)
+    private fun setUpTapToFocus(event: MotionEvent): Boolean {
+        return when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+                true
+            }
+            MotionEvent.ACTION_UP -> {
+                val factory: MeteringPointFactory = SurfaceOrientedMeteringPointFactory(
+                    viewFinder.width.toFloat(), viewFinder.height.toFloat()
+                )
+                val autoFocusPoint = factory.createPoint(event.x, event.y)
+                try {
+                    camera?.cameraControl?.startFocusAndMetering(
+                        FocusMeteringAction.Builder(
+                            autoFocusPoint,
+                            FocusMeteringAction.FLAG_AF
+                        ).apply {
+                            //focus only when the user tap the preview
+                            disableAutoCancel()
+                        }.build()
+                    )
+                } catch (e: CameraInfoUnavailableException) {
+                    Log.d("ERROR", "cannot access camera", e)
+                }
+                true
+            }
+            else -> false // Unhandled event.
+        }
+    }
 
-        viewFinder.setOnTouchListener { _, event ->
-            scaleGestureDetector.onTouchEvent(event)
-            return@setOnTouchListener true
+    fun setUpZoomAndFocus() {
+        var scaleGestureDetector: ScaleGestureDetector? = null
+        if (configs.pinchToZoomEnabled) {
+            val listener = object : ScaleGestureDetector.SimpleOnScaleGestureListener() {
+                override fun onScale(detector: ScaleGestureDetector): Boolean {
+                    val currentZoomRatio: Float = camera?.cameraInfo?.zoomState?.value?.zoomRatio ?: 0F
+                    val delta = detector.scaleFactor
+                    camera?.cameraControl?.setZoomRatio(currentZoomRatio * delta)
+                    zoomView.text = "Zoom: %.02f".format(currentZoomRatio)
+                    return true
+                }
+            }
+            scaleGestureDetector = ScaleGestureDetector(this, listener)
+        }
+        viewFinder.afterMeasured {
+            viewFinder.setOnTouchListener { _, event ->
+                return@setOnTouchListener when {
+                    configs.pinchToZoomEnabled && configs.focusMode == FOCUS_MODE_TAP -> setUpPinchToZoom(
+                        event,
+                        scaleGestureDetector
+                    ) && setUpTapToFocus(event)
+                    configs.pinchToZoomEnabled -> setUpPinchToZoom(event, scaleGestureDetector)
+                    configs.focusMode == FOCUS_MODE_TAP -> setUpTapToFocus(event)
+                    else -> false
+                }
+            }
+        }
+        if (configs.focusMode == FOCUS_MODE_AUTO) {
+            setAutomaticFocus()
+        } else if (configs.focusMode == FOCUS_MODE_OFF) {
+            camera?.cameraControl?.cancelFocusAndMetering()
         }
     }
 }
